218ad07f8c960e1131d10bf585a1554fe4d1c605
P-3-4 <amirpea4444@gmail.com>
2025-07-28 14:40:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '😎',
+  author: 'P34‎',
+  botName: "Edited Knight Bot",
+  botOwner: 'P34', // Your name
+  ownerNumber: '2348149404648', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Just a Bot😗.",
   version: "2.0.8",
 };
 
